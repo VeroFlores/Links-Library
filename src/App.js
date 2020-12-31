5daaf77b3ee4d387be8ac795d16434d95ac89e9a
VeroFlores <veritocalla@gmail.com>
2020-12-30 23:55:03 -0500

    developing crud function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootswatch/dist/pulse/bootstrap.min.css';
+import Links from './components/links'
+import { ToastContainer} from 'react-toastify';
+// componente que ayuda a mostrar 
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <Links/>
+      </div>
+      <ToastContainer/>
     </div>
-  );
+
+    );
 }
 
 export default App;
